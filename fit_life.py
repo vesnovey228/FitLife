88bec7026f730322a753c5e5ab1736aea2ed5d4d
Тимофей Подскрёбышев <vesnovey228@gmail.com>
2026-08-11 18:21:17 +0800

    Изменил код: теперь он проверяет вводимые пользователем данные, и перенос строк происходит с помощью \n вместо print''.

--- fit_life.py
+++ fit_life.py
@@ -1,15 +1,47 @@
 print('Здравствуйте! Пожалуйста, укажите ваше имя и возвраст.')
 user_name = input('Ваше имя: ')
-user_age = int(input('Ваш возвраст: '))
+while True:
+    try:
+        user_age = int(input('Ваш возвраст: '))
+        if user_age < 0:
+            print('Ваш возвраст не может быть меньше нуля. Попробуйте снова.')
+            continue
+        break
+    except ValueError:
+        print('Ошибка. Пожалуйста, введите число.')
+    except KeyboardInterrupt:
+        print('Программа завершина пользователем.')
+        break
 print('Пожалуйста, укажите ваш вес и рост.')
-user_weight = float(input('Ваш вес в кг (например, 63.3): '))
-user_height = float(input('Ваш рост в м (например, 1.73): '))
+while True:
+    try:
+        user_weight = float(input('Ваш вес в кг (например, 63.3): '))
+        if user_weight < 0:
+            print('Ваш вес не может быть отрицательным. Попробуйте снова.')
+            continue
+        break
+    except ValueError:
+        print('Ошибка. Пожалуйста, введите число.')
+    except KeyboardInterrupt:
+        print('Программа завершина пользователем.')
+        break
+while True:
+    try:
+        user_height = float(input('Ваш рост в м (например, 1.73): '))
+        if user_height < 0:
+            print('Ваш рост не может быть отрицательным. Попробуйте снова.')
+            continue
+        break
+    except ValueError:
+        print('Ошибка. Пожалуйста, введите число.')
+    except KeyboardInterrupt:
+        print('Программа завершина пользователем.')
+        break
 bmi = round(user_weight / (user_height ** 2), 1)  # расчёт индекса массы тела
-water_l_per_kg = 30 / 1000
+water_per_kg = 30
+water_l_per_kg = water_per_kg / 1000
 water_l = (user_weight * water_l_per_kg)  # расчёт рекомендуемой нормы воды
-print('')
-print(f'Ваш отчёт, {user_name} ({user_age} г.):')  # вывод результатов
+print(f'\nВаш отчёт, {user_name} ({user_age} г.):')
 print(f'Индекс массы тела: {bmi}')
-print(f'Рекомендуемая норма воды: {water_l:.2f} л. в день')
-print('')
+print(f'Рекомендуемая норма воды: {water_l:.2f} л. в день\n')
 print('Расчёт окончен. Будьте здоровы!')
